ec2ce5a671ff6f387debd12d69927acd4edb0049
Erik Valkering <erikvalkering@gmail.com>
2020-08-14 15:54:07 +0200

    Added support for `const` instructions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+struct Machine {
+    stack: Vec<i32>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let code = vec![
+        ("const", 1),
+        ("const", 2),
+    ];
+
+    let mut machine = Machine{
+        stack: Vec::new(),
+    };
+
+    for instruction in code {
+        match instruction {
+            ("const", value) => machine.stack.push(value),
+            (operator, _) => eprintln!("Unknown instruction: {}", operator),
+        }
+    }
 }
