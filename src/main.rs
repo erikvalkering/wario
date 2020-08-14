34b18e0fb856326cc304e92a4de10c31f9fe7112
Erik Valkering <erikvalkering@gmail.com>
2020-08-14 16:04:48 +0200

    Implemented Add instruction

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ struct Machine {
 
 enum Instruction {
     Const(i32),
+    Add,
 }
 
 fn main() {
     let code: Vec<Instruction> = vec![
         Instruction::Const(1),
         Instruction::Const(2),
+        Instruction::Add,
     ];
 
     let mut machine = Machine{
@@ -19,6 +21,13 @@ fn main() {
     for instruction in code {
         match instruction {
             Instruction::Const(value) => machine.stack.push(value),
+            Instruction::Add => {
+                let a = machine.stack.pop().unwrap();
+                let b = machine.stack.pop().unwrap();
+                machine.stack.push(a + b);
+            }
         }
     }
+
+    println!("Result: {}", machine.stack.pop().unwrap());
 }
