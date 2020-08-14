354343a01f81dbdb0a108ab0a7083aab3c103b84
Erik Valkering <erikvalkering@gmail.com>
2020-08-14 15:58:24 +0200

    Made instructions type safe

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ struct Machine {
     stack: Vec<i32>,
 }
 
+enum Instruction {
+    Const(i32),
+}
+
 fn main() {
-    let code = vec![
-        ("const", 1),
-        ("const", 2),
+    let code: Vec<Instruction> = vec![
+        Instruction::Const(1),
+        Instruction::Const(2),
     ];
 
     let mut machine = Machine{
@@ -14,8 +18,7 @@ fn main() {
 
     for instruction in code {
         match instruction {
-            ("const", value) => machine.stack.push(value),
-            (operator, _) => eprintln!("Unknown instruction: {}", operator),
+            Instruction::Const(value) => machine.stack.push(value),
         }
     }
 }
