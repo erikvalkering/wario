1abfba1b4ed5153530424283873a56754d5c300f
Erik Valkering <erikvalkering@gmail.com>
2021-01-28 23:58:10 +0100

    Now parsing the module's preamble

--- tests/parser.rs
+++ tests/parser.rs
@@ -1,12 +1,40 @@
+use std::fs::File;
+use std::io::Read;
+
+type Result = std::result::Result<(), String>;
+
+fn parse_preamble(file: &mut File) -> Result {
+    let mut data: [u8; 4] = [0; 4];
+    if let Err(err) = file.read(&mut data) {
+        return Err(format!("Unable to read preamble: {}", err));
+    }
+
+    match &data {
+        b"\0asm" => Ok(()),
+        _ => Err("Invalid preamble".to_owned()),
+    }
+}
+
+fn parse_module(mut file: &mut File) -> Result {
+    parse_preamble(&mut file)?;
+
+    Ok(())
+}
+
 #[test]
-fn parse_wasm() -> Result<(), std::io::Error> {
+fn parse_wasm() -> Result {
     use std::path::PathBuf;
     let path: PathBuf = [env!("CARGO_MANIFEST_DIR"), "tests", "input.wasm"]
         .iter()
         .collect();
 
-    use std::fs::File;
-    File::open(path)?;
+    let file = File::open(path);
+    let mut file = match file {
+        Ok(file) => file,
+        Err(err) => return Err(format!("Unable to open file: {}", err)),
+    };
+
+    parse_module(&mut file)?;
 
     Ok(())
 }
